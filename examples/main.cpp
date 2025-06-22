22f72276e5d2ffc84dd74ab7d662aefaed3d2ac1
Babiciu Iustin <167506994+justy41@users.noreply.github.com>
2025-06-22 23:05:39 +0300

    Add files via upload

---
+++ examples/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <memory>
+#include "sine.h"
+#include "TemplateState.h"
+#include "SecondState.h"
+
+int main(){
+    InitSineWindow(1280, 720, 640, 360, "game", FLAG_WINDOW_RESIZABLE, FLAG_WINDOW_TRANSPARENT);
+    RenderTexture2D target = LoadRenderTexture(gameWidth, gameHeight);
+    float scale = 0;
+    
+    // ======================================================== START ========================================================= //
+    SineStateManager manager = SineStateManager();
+    manager.add(std::make_unique<TemplateState>());
+    manager.add(std::make_unique<SecondState>());
+    
+    while(!WindowShouldClose()) {
+    // ======================================================== UPDATE ======================================================== //
+        scale = std::min((float)GetScreenWidth()/gameWidth, (float)GetScreenHeight()/gameHeight);
+        manager.update(GetFrameTime());
+        
+    // ========================================================= DRAW ========================================================= //
+        BeginTextureMode(target);
+            ClearBackground(RAYWHITE);
+            manager.draw();
+        EndTextureMode();
+        
+        DrawLetterBox(target, scale, gameWidth, gameHeight);
+    }
+    
+    manager.UnloadStates();
+    CloseWindow();
+}
