268502668e0aa67bf6f15d89f2f05a996fa7534b
Babiciu Iustin <167506994+justy41@users.noreply.github.com>
2025-06-27 18:53:46 +0300

    Added LDtk map drawing support

--- include/sine.h
+++ include/sine.h
@@ -7,6 +7,9 @@
 #include <memory>
 #include <typeindex>
 #include <functional>
+#include <algorithm>
+#include <unordered_map>
+#include <LDtkLoader/Project.hpp>
 #include "raylib.h"
 #include "raymath.h"
 
@@ -242,7 +245,8 @@ class SineStateManager;
 class SineState : public SineGroup
 {
 private:
-    
+    ldtk::IntRect r;
+    std::unordered_map<std::string, Texture2D> tilesets;
 public:
     SineStateManager* manager;
     int stateIndex;
@@ -251,6 +255,11 @@ public:
     float offsetX, offsetY;
     
     Camera2D camera;
+    
+    ldtk::Project ldtkProject;
+    const ldtk::World* world;
+    const ldtk::Level* level_0;
+    const ldtk::Layer* ground_layer;
     
     // Adds a heap allocated object in a std::vector<SineBasic*>
     //
@@ -288,9 +297,92 @@ public:
     //
     // NOTE: put this depending of the draw order as SineState::draw()
     virtual void draw() {
-        BeginMode2D(camera);
-            SineGroup::draw();
-        EndMode2D();
+        SineGroup::draw();
+    }
+    
+    // Loads a LDtk map
+    //
+    // NOTE: tilesets are loaded relative to the file path of the .ldtk file. File paths can be found in the .ldtk file.
+    // The tileset paths are printed in the command line to see.
+    void LoadLDtkMap(const char* tilemap_path) {
+        ldtkProject.loadFromFile(tilemap_path);
+        world = &ldtkProject.getWorld();
+        
+        // This block of code takes the tilemap_path and erases the map.ldtk part.
+        // After that, the tileset path from the ldtk layer is appended to it, forming the path to the tileset.
+        // EXAMPLE: tilemaps/map.ldtk + ../tilesets/tileset_1.png = tilemaps/../tilesets/tileset_1.png
+        for(const auto& level : world->allLevels()) {
+            for(const auto& layer : level.allLayers()) {
+                if(tilesets.find(layer.getTileset().name) == tilesets.end()) {
+                    std::string texture_file_name = layer.getTileset().path; // Load file path relative to the .ldtk file 
+                    std::string map_path = tilemap_path;
+                    for(int i = map_path.size()-1; i>=0; i--) {
+                        if(map_path[i-1] == '/') {      // When the '/' is next...
+                            map_path.erase(i);          // ...then erase.
+                            break;
+                        }
+                    }
+                    
+                    map_path.append(texture_file_name); // Combine the two file paths.
+                    tilesets.insert({layer.getTileset().name, LoadTexture(map_path.c_str())}); // Insert the name and load the tileset.
+                    std::cout<<"\nTILESET PATH:\n"<<map_path<<"\n\n";
+                }
+            }
+        }
+    }
+    
+    // Draws the entire LDtk map
+    void DrawLDtkMap() {
+        for(const auto& level : world->allLevels()) {
+            for(int i = level.allLayers().size()-1; i>=0; i--) { // Reversed order because LDtkLoader takes the layers inverted.
+                std::string ts_name = level.allLayers()[i].getTileset().name; // Get the tileset name of the current layer.
+                if(level.allLayers()[i].isVisible()) {
+                    for(const auto& tile : level.allLayers()[i].allTiles()) { // Draw all tiles based on their source rect.
+                        r = tile.getTextureRect(); // source rect
+                        DrawTextureRec(
+                            tilesets[ts_name],
+                            Rectangle{(float)r.x, (float)r.y, (float)r.width, (float)r.height},
+                            Vector2{(float)tile.getPosition().x + level.position.x, (float)tile.getPosition().y + level.position.y},
+                            WHITE
+                        );
+                    }
+                }
+            }
+        }
+    }
+    
+    // Draws only the named level
+    void DrawLDtkLevel(const char* level_name) {
+        for(int i = world->getLevel(level_name).allLayers().size()-1; i>=0; i--) {
+            std::string ts_name = world->getLevel(level_name).allLayers()[i].getTileset().name;
+            if(world->getLevel(level_name).allLayers()[i].isVisible()) {
+                for(const auto& tile : world->getLevel(level_name).allLayers()[i].allTiles()) {
+                    r = tile.getTextureRect();
+                    DrawTextureRec(
+                        tilesets[ts_name],
+                        Rectangle{(float)r.x, (float)r.y, (float)r.width, (float)r.height},
+                        Vector2{(float)tile.getPosition().x, (float)tile.getPosition().y},
+                        WHITE
+                    );
+                }
+            }
+        }
+    }
+    
+    // Draws a layer from all levels
+    void DrawLDtkLayer(const char* layer_name) {
+        for(const auto& level : world->allLevels()) {
+            std::string ts_name = level.getLayer(layer_name).getTileset().name;
+            for(const auto& tile : level.getLayer(layer_name).allTiles()) {
+                r = tile.getTextureRect();
+                DrawTextureRec(
+                    tilesets[ts_name],
+                    Rectangle{(float)r.x, (float)r.y, (float)r.width, (float)r.height},
+                    Vector2{(float)tile.getPosition().x + level.position.x, (float)tile.getPosition().y + level.position.y},
+                    WHITE
+                );
+            }
+        }
     }
     
     /// @brief Gets the virtual X mouse position, dependend on the scale of the window
